82f945d7215e86b61d9d4af5a0083592aa6982c6
AyushKumar2023 <ayushamber02@gmail.com>
2025-06-16 13:21:30 +0530

    upo adding doctors from database

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,17 +1,61 @@
-
 import { createContext, useState } from "react";
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 export const AdminContext= createContext()
 
 const AdminContextProvider=(props)=>{
 
     const [adminToken, setAdminToken]=useState(localStorage.getItem('adminToken')? localStorage.getItem('adminToken') : "")
+    const [doctors, setDoctors]=useState([])
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const getAllDoctors=async () =>{
+
+        try {
+            
+            // getting data from api response
+            const {data}=await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers:{atoken:adminToken}})
+
+            if(data.success){
+                setDoctors(data.doctors)
+                console.log(data.doctors);
+                
+            }else{
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const changeAvailability=async (docId)=>{
+
+        try {
+
+            const {data}=await axios.post(backendUrl + '/api/admin/change-availability', {docId}, {headers:{atoken:adminToken}})
+            // const { data } = await axios.post('http://localhost:4000/api/admin/change-availability', { docId }, { headers: { atoken: adminToken } })
+
+
+            if(data.success){
+                toast.success(data.message)
+                getAllDoctors()
+            }else{
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
  
     const value={
         adminToken, setAdminToken,
-        backendUrl
+        backendUrl, doctors, getAllDoctors,
+        changeAvailability
     }
 
     return (
