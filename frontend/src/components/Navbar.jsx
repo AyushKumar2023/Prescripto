e76d0296fb8619df3c0269324845dfc4fc2f1b62
AyushKumar2023 <ayushamber02@gmail.com>
2025-06-24 14:41:48 +0530

    user profile editing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
     const navigate=useNavigate()
     const [showMenu, setShowMenu]=useState(false)
     // const [token, setToken]=useState(true)
-    const {token, setToken}=useContext(AppContext)
+    const {token, setToken, userData}=useContext(AppContext)
 
     function navigateLogin(){
         navigate('/login')
@@ -44,9 +44,9 @@ function Navbar() {
         </ul>
         <div className='flex items-center gap-4'>
             {
-                token ? 
+                token && userData ? 
                 <div className='flex items-center cursor-pointer group relative'>
-                    <img src={assets.profile_pic} className='w-8 rounded-full' alt="" />
+                    <img src={userData.image} className='w-8 rounded-full' alt="" />
                     <img src={assets.dropdown_icon} className='w-2.5' alt="" />
                     <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
