78f77e975826b93c692aab169e034c03e03514b3
AyushKumar2023 <ayushamber02@gmail.com>
2025-07-03 13:45:08 +0530

    env

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
+import appointmentModel from '../models/appointmentModel.js'
 
 // API for adding doctor
 
@@ -96,5 +97,22 @@ const allDoctors=async (req, res)=>{
     }
 }
 
+// api to get all appointment lists
+const appointmentsAdmin= async (req, res)=>{
+    try {
 
-export {addDoctor, loginAdmin, allDoctors}
+        const appointments = await appointmentModel.find({})
+
+        res.json({
+            success:true,
+            appointments
+        })
+        
+    } catch (error) {
+         console.log(error);
+        res.json({success:false, message:error.message})
+    }
+}
+
+
+export {addDoctor, loginAdmin, allDoctors, appointmentsAdmin}
